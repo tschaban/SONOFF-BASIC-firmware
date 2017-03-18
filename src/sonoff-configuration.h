9db6b8732767d2b705f9b053a36ad71e7b4b9b21
tschaban <github@adrian.czabanowski.com>
2017-03-18 23:54:03 +0100

    added additional config params

--- src/sonoff-configuration.h
+++ src/sonoff-configuration.h
@@ -23,16 +23,30 @@
 #define   RELAY_OFF 0
 #define   RELAY_ON 1
 
+#define   SWITCH_SENSITIVENESS_HIGH 0
+#define   SWITCH_SENSITIVENESS_NORMAL 1
+#define   SWITCH_SENSITIVENESS_LOW 2
+#define   SWITCH_SENSITIVENESS_HIGH_VALUE 0
+#define   SWITCH_SENSITIVENESS_NORMAL_VALUE 3
+#define   SWITCH_SENSITIVENESS_LOW_VALUE 10 
+
+#define   GPIO_1 1
+#define   GPIO_3 3
+#define   GPIO_14 14
 
 struct DEFAULTS {
-  char          version[8] = "0.6.1";
+  char          version[8] = "0.7.0";
   float         temp_correction = 0;
   char          language[3] = "en";
   unsigned int  mqtt_port = 1883;
   unsigned int  temp_interval = 600;
   boolean       temp_present = false; 
-  uint8_t       relay_state_after_power_restored = 3;
-  uint8_t       relay_state_after_connection_restored = 4;        
+  uint8_t       relay_state_after_power_restored = DEFAULT_RELAY_LAST_KNOWN;
+  uint8_t       relay_state_after_connection_restored = DEFAULT_RELAY_SERVER;
+  boolean       switch_present = false;
+  uint8_t       switch_sensitiveness = SWITCH_SENSITIVENESS_NORMAL;
+  uint8_t       switch_gpio = GPIO_14;
+          
  };
 
 
@@ -47,6 +61,7 @@ struct SONOFFCONFIG {
   char          wifi_ssid[32];
   char          wifi_password[32];
 
+
   char          mqtt_host[32];
   unsigned int  mqtt_port;
   char          mqtt_user[32];
@@ -56,7 +71,13 @@ struct SONOFFCONFIG {
   float         ds18b20_correction;
   unsigned int  ds18b20_interval;
   boolean       ds18b20_present;
+  boolean       switch_present;
+  uint8_t       switch_sensitiveness;
+  uint8_t       switch_gpio;
+
+  boolean       debugger;
 };
 
 
 #endif
+
