5c561bfd89324c743db3cff82a7d17c777740ed9
tschaban <github@adrian.czabanowski.com>
2017-04-08 23:33:01 +0200

    post upgrade move to separate class

---
+++ src/sonoff-upgrade.h
@@ -0,0 +1,27 @@
+/*
+ SONOFF BASIC: firmware
+ More info: https://github.com/tschaban/SONOFF-BASIC-firmware
+ LICENCE: http://opensource.org/licenses/MIT
+ 2016-10-27 tschaban https://github.com/tschaban
+*/
+
+#ifndef _sonoff_upgrade_h
+#define _sonoff_upgrade_h
+
+
+class SonoffFirmware {
+  private:  
+    void v032();
+    void v040();
+    void v050();
+    void v061();
+    void v070();
+      
+  public:
+    SonoffFirmware();
+    boolean upgraded();
+    void update();
+
+};
+#endif
+
---
+++ src/sonoff-upgrade.ino
@@ -0,0 +1,97 @@
+/*
+  SONOFF BASIC: firmware
+  More info: https://github.com/tschaban/SONOFF-BASIC-firmware
+  LICENCE: http://opensource.org/licenses/MIT
+  2016-10-27 tschaban https://github.com/tschaban
+*/
+
+#include "sonoff-upgrade.h"
+
+SonoffFirmware::SonoffFirmware() {
+
+}
+
+boolean SonoffFirmware::upgraded() {
+  if (String(sonoffDefault.version) != String(Configuration.version)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+void SonoffFirmware::update() {
+  if (Configuration.debugger) Serial << endl << "WARN: Firmware was upgraded from version : " << Configuration.version << " to " << sonoffDefault.version;
+  Eeprom.saveVersion(sonoffDefault.version);
+
+  v032();
+  v040();
+  v050();
+  v061();
+  v070();
+
+  Configuration = Eeprom.getConfiguration();
+}
+
+
+void SonoffFirmware::v032() {
+  if (String(Configuration.version) == "0.3.2") {
+    Eeprom.saveRelayStateAfterPowerRestored(sonoffDefault.relay_state_after_power_restored);
+  }
+}
+
+
+void SonoffFirmware::v040() {
+  if (String(Configuration.version) == "0.3.2" ||
+      String(Configuration.version) == "0.4.0") {
+    Eeprom.saveLanguage(sonoffDefault.language);
+  }
+
+}
+void SonoffFirmware::v050() {
+  if (String(Configuration.version) == "0.3.2" ||
+      String(Configuration.version) == "0.4.0" ||
+      String(Configuration.version) == "0.5.0") {
+
+    char _id[6] = {0};
+    char _device_name[32] = {0};
+
+    sprintf(_id, "%06X", ESP.getChipId());
+    sprintf(_device_name, "SONOFF_%s", _id);
+    Eeprom.saveDeviceName(_device_name);
+
+    /* After Connecton is restored parameter is set to saved After Power is restored value */
+    Eeprom.saveRelayStateAfterConnectionRestored(Eeprom.getRelayStateAfterPowerRestored());
+
+    /* Value "Server value" [4] is not longer needed for parameter  After Power Restored.
+        If it was set like that  by a user it's upgraded to value 2 which is relay state OFF
+    */
+    if (Eeprom.getRelayStateAfterPowerRestored() == 4) {
+      Eeprom.saveRelayStateAfterPowerRestored(2);
+    }
+  }
+}
+
+void SonoffFirmware::v061() {
+  if (String(Configuration.version) == "0.3.2" ||
+      String(Configuration.version) == "0.4.0" ||
+      String(Configuration.version) == "0.5.0" ||
+      String(Configuration.version) == "0.6.1") {
+    Eeprom.saveSwitchPresent(sonoffDefault.switch_present);
+    Eeprom.saveSwitchGPIO(sonoffDefault.switch_gpio);
+    Eeprom.saveSwitchSensitiveness(sonoffDefault.switch_sensitiveness);
+    Eeprom.saveDebuggable(0);
+  }
+}
+
+void SonoffFirmware::v070() {
+  if (String(Configuration.version) == "0.3.2" ||
+      String(Configuration.version) == "0.4.0" ||
+      String(Configuration.version) == "0.5.0" ||
+      String(Configuration.version) == "0.6.1" ||
+      String(Configuration.version) == "0.7.0")
+  {
+    Eeprom.saveInterface(1);
+  }
+
+}
+
