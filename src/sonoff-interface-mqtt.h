05949617b6e96311bd3d79a939d10afecfe3ad1b
tschaban <github@adrian.czabanowski.com>
2017-04-08 23:34:18 +0200

    MQTT class created

---
+++ src/sonoff-interface-mqtt.h
@@ -0,0 +1,26 @@
+/*
+ SONOFF BASIC: firmware
+ More info: https://github.com/tschaban/SONOFF-BASIC-firmware
+ LICENCE: http://opensource.org/licenses/MIT
+ 2016-10-27 tschaban https://github.com/tschaban
+*/
+
+#ifndef _sonoff_interface_mqtt_h
+#define _sonoff_interface_mqtt_h
+
+#include <PubSubClient.h>
+
+class SonoffMQTTInterface {
+  private:
+    PubSubClient Broker;
+
+  public:
+    SonoffMQTTInterface();
+    void begin();
+    void connect();
+    void publish(char* type, char* message);
+    boolean connected();
+    void loop();
+
+};
+#endif
---
+++ src/sonoff-interface-mqtt.ino
@@ -0,0 +1,81 @@
+/*
+  SONOFF BASIC: firmware
+  More info: https://github.com/tschaban/SONOFF-BASIC-firmware
+  LICENCE: http://opensource.org/licenses/MIT
+  2016-10-27 tschaban https://github.com/tschaban
+*/
+
+#include "sonoff-interface-mqtt.h"
+
+SonoffMQTTInterface::SonoffMQTTInterface() {
+}
+
+void SonoffMQTTInterface::begin() {
+  Broker.setClient(esp);
+  Broker.setServer(Configuration.mqtt_host, Configuration.mqtt_port);
+  Broker.setCallback(callbackMQTT);
+}
+
+
+void SonoffMQTTInterface::connect() {
+  char  mqttString[60];
+
+  sprintf(mqttString, "Sonoff (Device name: %s)", Configuration.device_name);
+  if (Configuration.debugger) Serial << endl << "INFO: Connecting to MQTT : " << Configuration.mqtt_host << ":" << Configuration.mqtt_port;
+  
+  while (!Broker.connected()) {    
+    if (Broker.connect(mqttString, Configuration.mqtt_user, Configuration.mqtt_password)) {
+      if (Configuration.debugger) Serial << endl << "INFO: Connected";
+      sprintf(mqttString, "%scmd", Configuration.mqtt_topic);
+      Broker.subscribe(mqttString);
+      if (Configuration.debugger) Serial << endl << "INFO: Subsribed to : " << Configuration.mqtt_topic;      
+    } else {
+      delay(CONNECTION_WAIT_TIME);
+      if (Configuration.debugger) Serial << endl << "INFO: MQTT connection status: " << Broker.state();
+    }
+  }
+}
+
+boolean SonoffMQTTInterface::connected() {
+  return Broker.connected();
+}
+
+void SonoffMQTTInterface::publish(char* type, char* message) {
+  if (Broker.state() == MQTT_CONNECTED) {
+      char  mqttTopic[50];
+      sprintf(mqttTopic, "%s%s", Configuration.mqtt_topic,type);
+      Broker.publish(mqttTopic, message);
+      if (Configuration.debugger) Serial << endl << "INFO: MQTT publising to:  " << mqttTopic << "  \\ " << message;
+  } else {
+      if (Configuration.debugger) Serial << endl << "WARN: MQTT not connected. State:  " << Broker.state();
+  }
+}
+
+void  SonoffMQTTInterface::loop() {
+  Broker.loop();
+}
+
+/* Callback of MQTT Broker, it listens for messages */
+void callbackMQTT(char* topic, byte* payload, unsigned int length) {
+  Led.blink();
+  if (Configuration.debugger) Serial << endl << "INFO: MQTT message recieved: " << topic << " \\";
+  if (length >= 1) { // command arrived
+    if ((char)payload[1] == 'N') { // ON
+      if (Configuration.debugger) Serial << " ON";
+      Relay.on();
+    } else if ((char)payload[1] == 'F') { // OFF
+      if (Configuration.debugger) Serial << " OFF";
+      Relay.off();
+    } else if ((char)payload[2] == 'p') { // reportState
+      if (Configuration.debugger) Serial << " reportState";
+      Relay.publish();
+    } else if ((char)payload[2] == 'b') { // reboot
+      if (Configuration.debugger) Serial << " reboot";
+      ESP.restart();
+    } else if ((char)payload[2] == 'n') { // configurationMode
+      if (Configuration.debugger) Serial << " configuration Mode";
+      Sonoff.toggle();
+    }
+  }
+}
+
